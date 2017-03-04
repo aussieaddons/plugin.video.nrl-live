cfc4d389a5bbfab60e51175818e97b5afff16a64
Glenn Guy <glennguy83@gmail.com>
2017-03-04 14:07:28 +1100

    Ooyala cleanup, cache user token

--- resources/lib/ooyalahelper.py
+++ resources/lib/ooyalahelper.py
@@ -51,19 +51,18 @@ username = addon.getSetting('LIVE_USERNAME')
 password = addon.getSetting('LIVE_PASSWORD')
 
 
-def fetch_nrl_xml(url, data):
-    """ send http POST and return the xml response as string with
-        removed utf-8 BOM"""
-    req = session.post(url, data=data, headers=config.YINZCAM_AUTH_HEADERS, verify=False)
-    utils.log("Fetching URL: {0}".format(url))
-    return req.text[1:]
-    
 def get_nrl_user_token():
     """send user login info and retrieve user id for session"""
+    stored_token = addon.getSetting('TOKEN')
+    if stored_token != '':
+        return stored_token
+    
     free_sub = int(addon.getSetting('SUBSCRIPTION_TYPE'))
     
     if free_sub:
-        return telstra_auth.get_free_token(username, password)
+        token = telstra_auth.get_free_token(username, password)
+        addon.setSetting('TOKEN', token)
+        return token
     
     login_resp = telstra_auth.get_paid_token(username, password)
     json_data = json.loads(login_resp)
@@ -74,24 +73,26 @@ def get_nrl_user_token():
             raise Exception('Please check your username '
                             'and password in the settings')
         raise Exception(json_data.get('ErrorMessage'))
-    return json_data.get('UserToken')
+    token = json_data.get('UserToken')
+    addon.setSetting('TOKEN', token)
+    return token
     
 
-def create_nrl_userid_xml(userId):
+def create_nrl_userid_xml(user_id):
     """ create a small xml file to send with http POST 
         when starting a new video request"""
     root = ET.Element('Subscription')
     ut = ET.SubElement(root, 'UserToken')
-    ut.text = userId
+    ut.text = user_id
     fakefile = StringIO.StringIO()
     tree = ET.ElementTree(root)
     tree.write(fakefile, encoding='UTF-8')
     output = fakefile.getvalue()
     return output
 
-def fetch_nrl_smil(videoId):
+def fetch_nrl_smil(video_id):
     """ contact ooyala server and retrieve smil data to be decoded"""
-    url = config.SMIL_URL.format(videoId)
+    url = config.SMIL_URL.format(video_id)
     utils.log("Fetching URL: {0}".format(url))
     res = session.get(url)
     return res.text
@@ -107,28 +108,39 @@ def get_nrl_hds_url(encryptedSmil):
 
 def get_nrl_embed_token(userToken, videoId):
     """send our user token to get our embed token, including api key"""
-    xml = fetch_nrl_xml(config.EMBED_TOKEN_URL.format(videoId), 
-                    create_nrl_userid_xml(userToken))
-    tree = ET.fromstring(xml)
-    return tree.find('Token').text
+    data = create_nrl_userid_xml(userToken)
+    url = config.EMBED_TOKEN_URL.format(videoId)
+    utils.log("Fetching URL: {0}".format(url))
+    try:
+        req = session.post(url, data=data, headers=config.YINZCAM_AUTH_HEADERS, verify=False)
+        xml = req.text[1:]
+        tree = ET.fromstring(xml)
+        if tree.find('ErrorCode') is not None:
+            utils.log('ERrorcode not found')
+            raise NRLException()
+        token = tree.find('Token').text
+    except NRLException:
+        addon.setSetting('TOKEN', '')
+        raise Exception('Login token has expired, please try again')
+    return token
 
 #common ooyala functions
  
-def get_secure_token(secureUrl, videoId):
+def get_secure_token(secure_url, videoId):
     """send our embed token back with a few other url encoded parameters"""
-    res = session.get(secureUrl)
+    res = session.get(secure_url)
     data = res.text
     parsed_json = json.loads(data)
-    iosToken =  parsed_json['authorization_data'][videoId]['streams'][0]['url']['data']
-    return base64.b64decode(iosToken)
+    token =  parsed_json['authorization_data'][videoId]['streams'][0]['url']['data']
+    return base64.b64decode(token)
 
-def get_m3u8_streams(secureTokenUrl):
+def get_m3u8_streams(secure_token_url):
     """ fetch our m3u8 file which contains streams of various qualities"""
-    res = session.get(secureTokenUrl)
+    res = session.get(secure_token_url)
     data = res.text.splitlines()
     return data
    
-def parse_m3u8_streams(data, live, secureTokenUrl):
+def parse_m3u8_streams(data, live, secure_token_url):
     """ Parse the retrieved m3u8 stream list into a list of dictionaries
         then return the url for the highest quality stream. Different 
         handling is required of live m3u8 files as they seem to only contain
@@ -147,8 +159,8 @@ def parse_m3u8_streams(data, live, secureTokenUrl):
     if '#EXT-X-VERSION:3' in data:
         data.remove('#EXT-X-VERSION:3')
     count = 1
-    m3uList = []
-    prependLive = secureTokenUrl[:secureTokenUrl.find('index-root')]
+    m3u_list = []
+    prepend_live = secure_token_url[:secure_token_url.find('index-root')]
     while count < len(data):
         line = data[count]
         line = line.strip('#EXT-X-STREAM-INF:')
@@ -165,13 +177,13 @@ def parse_m3u8_streams(data, live, secureTokenUrl):
         if not live:
             linelist.append(['URL',data[count+1]])
         else:
-            linelist.append(['URL',prependLive+data[count+1]])
+            linelist.append(['URL',prepend_live+data[count+1]])
         
-        m3uList.append(dict((i[0], i[1]) for i in linelist))
+        m3u_list.append(dict((i[0], i[1]) for i in linelist))
         count += 2
     
-    sorted_m3uList = sorted(m3uList, key=lambda k: int(k['BANDWIDTH']))
-    stream = sorted_m3uList[qual]['URL']
+    sorted_m3u_list = sorted(m3u_list, key=lambda k: int(k['BANDWIDTH']))
+    stream = sorted_m3u_list[qual]['URL']
     return stream
    
 def get_m3u8_playlist(video_id, live):
